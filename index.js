11612d9cf324cfe7b60796926d58270a0eb5339b
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-08-31 20:25:12 +0000

    solution 1

--- index.js
+++ index.js
@@ -3,5 +3,28 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    
+    let i=0,j=0,k=0
+    let result = []
+    const tester = {}
+    for (i in nums){
+        if (j!=i){
+            for (j in nums){
+                if (k!=j){
+                    for (k in nums){
+                        if (k!=i){
+                            if (nums[i]+nums[j]+nums[k]===0){
+                                let temp = [nums[i],nums[j],nums[k]].sort()
+                                if (!tester[temp]){
+                                    tester[temp]=1
+                                    result.push(temp)
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+    return(result)
 };
